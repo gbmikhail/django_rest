a949e877e34a3e819d393ccb43d41bff06380a7e
gbmikhail <vas1024@mail.ru>
2021-05-25 00:16:41 +0300

    Практическое задание к Урок 4. Views. Filtering. Pagination

--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -1,15 +1,50 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
+from todo.filters import ProjectFilter
 from todo.models import Project, Todo
 from todo.serializers import ProjectModelSerializer, TodoModelSerializer
+
+
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
 
 
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    pagination_class = ProjectLimitOffsetPagination
+    filterset_class = ProjectFilter
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given project,
+        by filtering against a `name` query parameter in the URL.
+        """
+        queryset = Project.objects.all()
+        name = self.request.query_params.get('name')
+        if name is not None:
+            queryset = queryset.filter(name__contains=name)
+        return queryset
 
 
 class TodoModelViewSet(ModelViewSet):
+    class TodoLimitOffsetPagination(LimitOffsetPagination):
+        default_limit = 20
+
     queryset = Todo.objects.all()
     serializer_class = TodoModelSerializer
+    pagination_class = TodoLimitOffsetPagination
+    filterset_class = ProjectFilter
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def perform_destroy(self, instance):
+        instance.is_active = False
+        instance.save()
