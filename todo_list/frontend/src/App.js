e158316c094f16d4315b6e3bb0740bc36a7cbb1d
gbmikhail <vas1024@mail.ru>
2021-05-27 00:35:55 +0300

    Практическое задание к Урок 5. Routing. SPA

--- todo_list/frontend/src/App.js
+++ todo_list/frontend/src/App.js
@@ -2,26 +2,51 @@ import './App.css';
 import './css/dropdown.css';
 import React from 'react';
 import axios from 'axios'
+import {Route, Link, Switch, BrowserRouter} from 'react-router-dom'
 import UserList from "./components/users";
 import Footer from "./components/footer";
 import Menu from "./components/menu";
+import ProjectList from "./components/projects";
+import TodoList from "./components/todo";
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            'users': []
+            'users': [],
+            'projects': [],
+            'todo': [],
         }
     }
 
     componentDidMount() {
-        axios.get('http://127.0.0.1:8000/api/users')
+        const url = 'http://127.0.0.1:8000/api'
+        axios.get(`${url}/user`)
             .then(response => {
                 const users = response.data
-                console.log(users)
                 this.setState(
                     {
                         'users': users
+                    }
+                )
+            }).catch(error => console.log(error))
+
+        axios.get(`${url}/project`)
+            .then(response => {
+                const projects = response.data
+                this.setState(
+                    {
+                        'projects': projects
+                    }
+                )
+            }).catch(error => console.log(error))
+
+        axios.get(`${url}/todo`)
+            .then(response => {
+                const todo = response.data
+                this.setState(
+                    {
+                        'todo': todo
                     }
                 )
             }).catch(error => console.log(error))
@@ -30,13 +55,24 @@ class App extends React.Component {
     render() {
         return (
             <body>
+            <BrowserRouter>
                 <Menu/>
 
                 <div className="content">
-                    <UserList users={this.state.users} />
+                    {/*<UserList users={this.state.users} />*/}
+                    {/*<ProjectList projects={this.state.projects} />*/}
+                    {/*<TodoList todos={this.state.todo} />*/}
+
+                    <Switch>
+                        <Route exact path='/' component={() => <UserList users={this.state.users} />} />
+                        <Route exact path='/user' component={() => <UserList users={this.state.users} />} />
+                        <Route exact path='/project' component={() => <ProjectList projects={this.state.projects} />} />
+                        <Route exact path='/todo' component={() => <TodoList todos={this.state.todo} />} />
+                    </Switch>
                 </div>
 
                 <Footer/>
+            </BrowserRouter>
             </body>
         )
     }
--- todo_list/frontend/src/components/menu.js
+++ todo_list/frontend/src/components/menu.js
@@ -1,19 +1,27 @@
 import '../App.css';
 
+import {HashRouter, Route, Link} from 'react-router-dom'
+
 const Menu = () => {
    return (
         <div className="navbar">
-            <a href="#">Главная</a>
-            <div className="dropdown">
-                <button className="dropbtn">Выпадающее меню
-                    {/*<i className="fa-caret-down"></i>*/}
-                </button>
-                <div className="dropdown-content">
-                    <a href="#">Link 1</a>
-                    <a href="#">Link 2</a>
-                    <a href="#">Link 3</a>
-                </div>
-            </div>
+            <Link to='/'>Главная</Link>
+
+            <Link to='/user'>Пользователи</Link>
+            <Link to='/project'>Проекты</Link>
+            <Link to='/todo'>Заметки</Link>
+
+
+            {/*<div className="dropdown">*/}
+            {/*    <button className="dropbtn">Выпадающее меню*/}
+            {/*        /!*<i className="fa-caret-down"></i>*!/*/}
+            {/*    </button>*/}
+            {/*    <div className="dropdown-content">*/}
+            {/*        <Link to='/user'>Пользователи</Link>*/}
+            {/*        <Link to='/project'>Проекты</Link>*/}
+            {/*        <Link to='/todo'>Заметки</Link>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
         </div>
    )
 }
