4406de10fe15e3c68ecc99e5cd785d25b6cd6e35
gbmikhail <vas1024@mail.ru>
2021-06-04 19:07:54 +0300

    Практическое задание к Урок 7. Авторизация на стороне клиента

--- todo_list/frontend/src/App.js
+++ todo_list/frontend/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import './css/dropdown.css';
 import React from 'react';
-import axios from 'axios'
+import axios from 'axios';
+import Cookies from 'universal-cookie';
 import {Route, Link, Switch, BrowserRouter} from 'react-router-dom'
 import UserList from "./components/users";
 import Footer from "./components/footer";
 import Menu from "./components/menu";
 import ProjectList from "./components/projects";
 import TodoList from "./components/todo";
+import LoginForm from "./components/Auth";
 
 class App extends React.Component {
     constructor(props) {
@@ -16,12 +18,40 @@ class App extends React.Component {
             'users': [],
             'projects': [],
             'todo': [],
+            'token': '',
+            'login': '',
+        }
+    }
+
+    create_headers() {
+        if (!this.is_auth()) {
+            console.log('empty', this.state.token)
+            return {}
+        }
+
+        return {
+            'Authorization': `Token ${this.state.token}`
         }
     }
 
     componentDidMount() {
+        const cookies = new Cookies();
+
+        this.setState(
+            {
+                'token': cookies.get('token'),
+                'login': cookies.get('login'),
+            })
+        this.state.token = cookies.get('token')
+        this.load_data()
+    }
+
+    load_data() {
+        const headers = this.create_headers()
+        console.log('xxxxxxxxxxxxxxx')
+        console.log(headers)
         const url = 'http://127.0.0.1:8000/api'
-        axios.get(`${url}/user`)
+        axios.get(`${url}/user`, {headers})
             .then(response => {
                 const users = response.data
                 this.setState(
@@ -31,7 +61,7 @@ class App extends React.Component {
                 )
             }).catch(error => console.log(error))
 
-        axios.get(`${url}/project`)
+        axios.get(`${url}/project`, {headers})
             .then(response => {
                 const projects = response.data
                 this.setState(
@@ -41,7 +71,7 @@ class App extends React.Component {
                 )
             }).catch(error => console.log(error))
 
-        axios.get(`${url}/todo`)
+        axios.get(`${url}/todo`, {headers})
             .then(response => {
                 const todo = response.data
                 this.setState(
@@ -52,11 +82,51 @@ class App extends React.Component {
             }).catch(error => console.log(error))
     }
 
+    is_auth() {
+        return this.state.token !== ''
+    }
+
+    logout() {
+        this.setState(
+            {
+                'token': ''
+            }
+        )
+        const cookies = new Cookies()
+        cookies.set('token', '')
+        cookies.set('login', '')
+    }
+
+    get_token(username, password) {
+        axios.post('http://127.0.0.1:8000/api-token-auth/', {username: username, password: password})
+            .then(response => {
+                console.log(response.status, response.data)
+                if (response.status === 200) {
+                    const token = response.data.token
+                    this.setState(
+                        {
+                            'token': token
+                        }
+                    )
+                    const cookies = new Cookies()
+                    cookies.set('token', token)
+                    cookies.set('login', username)
+                }
+            }).catch(error => alert(error))
+    }
+
     render() {
         return (
             <body>
             <BrowserRouter>
-                <Menu/>
+                <div className="navbar">
+                    <Link to='/'>Главная</Link>
+
+                    <Link to='/user'>Пользователи</Link>
+                    <Link to='/project'>Проекты</Link>
+                    <Link to='/todo'>Заметки</Link>
+                    {this.is_auth() ? <Link to={this.props.myroute} onClick={()=>this.logout()}>Выйти ({this.state.login})</Link> : <Link to='/login'>Авторизация</Link>}
+                </div>
 
                 <div className="content">
                     {/*<UserList users={this.state.users} />*/}
@@ -68,6 +138,7 @@ class App extends React.Component {
                         <Route exact path='/user' component={() => <UserList users={this.state.users} />} />
                         <Route exact path='/project' component={() => <ProjectList projects={this.state.projects} />} />
                         <Route exact path='/todo' component={() => <TodoList todos={this.state.todo} />} />
+                        <Route exact path='/login' component={() => <LoginForm get_token={(username, password) => this.get_token(username, password)} />} />} />
                     </Switch>
                 </div>
 
--- todo_list/frontend/src/components/menu.js
+++ todo_list/frontend/src/components/menu.js
@@ -1,30 +1,34 @@
 import '../App.css';
 
 import {HashRouter, Route, Link} from 'react-router-dom'
+import React from "react";
 
-const Menu = () => {
-   return (
-        <div className="navbar">
-            <Link to='/'>Главная</Link>
+ const Menu = (app) => {
+// class Menu extends React.Component {
+//
+//     render() {
+        return (
+            <div className="navbar">
+                <Link to='/'>Главная</Link>
 
-            <Link to='/user'>Пользователи</Link>
-            <Link to='/project'>Проекты</Link>
-            <Link to='/todo'>Заметки</Link>
+                <Link to='/user'>Пользователи</Link>
+                <Link to='/project'>Проекты</Link>
+                <Link to='/todo'>Заметки</Link>
+                {app.app.is_auth() ? <Link to='/logout'>Выйти</Link> : <Link to='/login'>Авторизация</Link>}
 
-
-            {/*<div className="dropdown">*/}
-            {/*    <button className="dropbtn">Выпадающее меню*/}
-            {/*        /!*<i className="fa-caret-down"></i>*!/*/}
-            {/*    </button>*/}
-            {/*    <div className="dropdown-content">*/}
-            {/*        <Link to='/user'>Пользователи</Link>*/}
-            {/*        <Link to='/project'>Проекты</Link>*/}
-            {/*        <Link to='/todo'>Заметки</Link>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
-        </div>
-   )
+                {/*<div className="dropdown">*/}
+                {/*    <button className="dropbtn">Выпадающее меню*/}
+                {/*        /!*<i className="fa-caret-down"></i>*!/*/}
+                {/*    </button>*/}
+                {/*    <div className="dropdown-content">*/}
+                {/*        <Link to='/user'>Пользователи</Link>*/}
+                {/*        <Link to='/project'>Проекты</Link>*/}
+                {/*        <Link to='/todo'>Заметки</Link>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
+            </div>
+        )
+    //}
 }
-
 
 export default Menu
